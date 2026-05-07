9c83e5737b4e8b2e80d0a71168cc2bbff9e6db57
James Teuscher <james.teuscher@outlook.com>
2026-05-06 22:25:13 -0600

    Add Exercise fetch skeleton

---
+++ exercise-web/src/ExerciseFetch.tsx
@@ -0,0 +1,12 @@
+import { Card, CardDescription, CardHeader, CardTitle } from "./components/ui/card";
+
+export function ExerciseFetch() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Fetch an Exercise</CardTitle>
+        <CardDescription>Specify a Muscle group and hit the button to </CardDescription>
+      </CardHeader>
+    </Card>
+  )
+}
