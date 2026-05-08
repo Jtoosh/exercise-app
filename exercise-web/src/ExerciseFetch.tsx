298399a27a5baaad4c544123ae318c6f847016c0
James Teuscher <james.teuscher@outlook.com>
2026-05-07 23:18:44 -0600

    Experiments with UI options

--- exercise-web/src/ExerciseFetch.tsx
+++ exercise-web/src/ExerciseFetch.tsx
@@ -1,4 +1,14 @@
-import { Card, CardDescription, CardHeader, CardTitle } from "./components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "./components/ui/card";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 export function ExerciseFetch() {
   return (
@@ -7,6 +17,33 @@ export function ExerciseFetch() {
         <CardTitle>Fetch an Exercise</CardTitle>
         <CardDescription>Specify a Muscle group and hit the button to </CardDescription>
       </CardHeader>
+      <CardContent>
+        <Select>
+          <SelectTrigger className="w-45">
+            <SelectValue placeholder="Muscle Group" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel className="SelectLabel">Upper Body</SelectLabel>
+              <SelectItem value="biceps">Biceps</SelectItem>
+              <SelectItem value="triceps">Triceps</SelectItem>
+              <SelectItem value="chest">Chest</SelectItem>
+              <SelectItem value="lats">Lats</SelectItem>
+              <SelectItem value="traps">Traps</SelectItem>
+            </SelectGroup>
+
+            <SelectSeparator></SelectSeparator>
+            
+            <SelectGroup>
+              <SelectLabel className="SelectLabel">Lower Body</SelectLabel>
+              <SelectItem value="quadriceps">Quads</SelectItem>
+              <SelectItem value="hamstrings">Hamstrings</SelectItem>
+              <SelectItem value="glutes">Glutes</SelectItem>
+              <SelectItem value="calves">Calves</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </CardContent>
     </Card>
   )
 }
