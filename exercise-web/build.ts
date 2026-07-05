22f69c3cf22a0dc06f25899062f9c1551e77ba9f
James Teuscher <james.teuscher@outlook.com>
2026-07-05 07:42:05 -0600

    fix: adjustments to CSS bundling, make available locally

--- exercise-web/build.ts
+++ exercise-web/build.ts
@@ -7,8 +7,7 @@ const outdir = path.join(process.cwd(), "dist");
 await rm(outdir, { recursive: true, force: true });
 
 const entrypoints = [...new Bun.Glob("src/**/*.html").scanSync()];
-
-const result = await Bun.build({
+ await Bun.build({
   entrypoints,
   outdir,
   minify: true,
@@ -19,16 +18,16 @@ const result = await Bun.build({
   },
 });
 
-await $`bunx @tailwindcss/cli -i ./src/index.css -o ./dist/output.css --minify`;
-
-const indexPath = path.join(outdir, "index.html");
-const html = await Bun.file(indexPath).text();
-const updated = html.replace(
-  "</head>",
-  '<link rel="stylesheet" href="./output.css">\n</head>'
-);
-await Bun.write(indexPath, updated);
-
-for (const output of result.outputs) {
-  console.log(` ${path.relative(process.cwd(), output.path)}  ${(output.size / 1024).toFixed(1)} KB`);
-}
+await $`bunx @tailwindcss/cli -i ./src/index.css -o ./styles/output.css --minify`;
+//
+// const indexPath = path.join(outdir, "index.html");
+// const html = await Bun.file(indexPath).text();
+// const updated = html.replace(
+//   "</head>",
+//   '<link rel="stylesheet" href="./output.css">\n</head>'
+// );
+// await Bun.write(indexPath, updated);
+//
+// for (const output of result.outputs) {
+//   console.log(` ${path.relative(process.cwd(), output.path)}  ${(output.size / 1024).toFixed(1)} KB`);
+// }
