8178e8b35beb8f01ba362c7a90a6aa4e1820723b
James Teuscher <james.teuscher@outlook.com>
2026-07-23 23:40:18 -0600

    fix: correct API url after deploying to vercel

--- exercise-web/src/service/ExerciseService.ts
+++ exercise-web/src/service/ExerciseService.ts
@@ -2,7 +2,7 @@ import {Exercise} from "@/lib/exercise.ts";
 
 export class ExerciseService {
     public getExerciseByMuscle = async (muscle: String): Promise<Exercise> => {
-        const results = await fetch(`/api/exercises?muscle=${muscle}`);
+        const results = await fetch(`https://exercise-app-blue-three.vercel.app/api/exercises?muscle=${muscle}`);
         const data = await results.json() as Exercise[];
         const index = Math.floor(Math.random() * data.length);
         return data[index]!
